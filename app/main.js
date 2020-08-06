4e89f2d132b732181fe982cd74267700181ddd83
Matt Mulit <mattmulit@hotmail.com>
2020-08-06 11:40:11 -0400

    organized and cleaned up comments

--- app/main.js
+++ app/main.js
@@ -6,30 +6,23 @@ const { app, BrowserWindow, dialog, ipcMain, Tray, Menu, globalShortcut } = requ
 let mainWindow = null;
 let tray = null;
 
-console.log(`${__dirname} is the __dirname`);
-console.log(path.relative(__dirname, '/assets'));
-
 const getFileFromUser = () => {
     const files = dialog.showOpenDialog({
         defaultPath: path.join(__dirname, '../assets'),
         properties: ['openFile'],
-        // buttonLabel: 'Unveil',
         title: 'Set Background Cheatsheet',
         filters: [
             { name: 'JPEG', extensions: ['jpg'] }
         ]
     });
     if (!files) return;
-    // console.log(files);
     const file = files[0];
     console.log("Opened: " + file);
     mainWindow.show();
-    // const content = fs.readFileSync(file).toString();
-    // console.log(content);
 };
 
 app.on('ready', () => {
-    const { screen } = require('electron');
+    const { screen } = require('electron'); // could not require() at the top of file, but this worked around
 
     const contextMenu = Menu.buildFromTemplate([{
         label: 'Set Background Cheatsheet',
@@ -39,49 +32,32 @@ app.on('ready', () => {
         },
     }]);
 
-    globalShortcut.register('Shift+Alt+2', () => {
-        // console.log('Shift+Alt+2 is pressed');
-        getFileFromUser();
-    });
-
     const { width: screenWidth, height: screenHeight } = screen.getPrimaryDisplay().workAreaSize;
-    console.log(screenWidth);
-    console.log(screenHeight);
+
     const mainWindowHeight = 800;
     const mainWindowWidth = 800;
 
     const mainWindowX = screenWidth / 2 - mainWindowWidth / 2;
     const mainWindowY = screenHeight / 2 - mainWindowHeight / 2;
 
+    mainWindow = new BrowserWindow({ show: true, width: mainWindowWidth, height: mainWindowHeight, x: mainWindowX, y: mainWindowY }); // webPreferences: { nodeIntegration: true },
+    mainWindow.loadFile(`${__dirname}/index.html`);
 
+    globalShortcut.register('Shift+Alt+2', () => {
+        getFileFromUser();
+    });
 
     tray = new Tray('Google-Chrome-Google-Chrome.ico');
     tray.setToolTip('~~MAKE NEW ICON~~');
     tray.setContextMenu(contextMenu);
-
-    mainWindow = new BrowserWindow({ show: true, width: mainWindowWidth, height: mainWindowHeight, x: mainWindowX, y: mainWindowY }); // webPreferences: { nodeIntegration: true },
-    mainWindow.loadFile(`${__dirname}/index.html`);
-
-    // console.log(tray.getBounds()); // { x: 2248, y: 1688, width: 32, height: 40 }
-    console.log(mainWindow.getBounds());
 
     // mainWindow.once('ready-to-show', () => {
     //     mainWindow.show();
     // });
 });
 
-
 ipcMain.on('asynchronous-message', (event, arg) => {
-    console.log(arg); // prints "ping"
+    console.log(arg); // "ping"
     getFileFromUser();
     event.sender.send('asynchronous-reply', 'pong');
-    // event.sender.send('pong');
 });
-
-// // Synchronous example only
-// ipcMain.on('synchronous-message', (event, arg) => {
-//     setTimeout(() => {
-//         console.log(arg); // prints "pingy"
-//         event.returnValue = 'pongy';
-//     }, 3000);
-// });
--- app/renderer.js
+++ app/renderer.js
@@ -3,31 +3,8 @@ const { ipcRenderer } = require('electron');
 const openFileButton = document.querySelector('#open-file');
 
 openFileButton.addEventListener('click', () => {
-    // console.log(arg); // prints "pingy"
-    // event.returnValue = 'pongy';
     ipcRenderer.send('asynchronous-message', 'ping');
-    // console.log('cool');
-    ipcRenderer.on('asynchronous-reply', (event, arg) => {
-        console.log(arg); // prints "pong"
+    ipcRenderer.on('asynchronous-reply', (event, arg) => { // event not used
+        console.log(arg); // "pong"
     });
 });
-
-// // Synchronous example only
-// console.log(ipcRenderer.sendSync('synchronous-message', 'pingy')); // prints "pongy"
-
-// // //
-
-// const marked = require('marked');
-
-// const markdownView = document.querySelector('#markdown');
-// const htmlView = document.querySelector('#html');
-
-// const renderMarkdownToHtml = markdown => {
-//     htmlView.innerHTML = marked(markdown, { sanitize: true });
-// };
-
-// markdownView.addEventListener('keyup', event => {
-//     const currentContent = event.target.value;
-//     renderMarkdownToHtml(currentContent);
-// });
-// };
