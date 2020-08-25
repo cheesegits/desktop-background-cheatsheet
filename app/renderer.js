9682ec07bdfeef54386ab6be03bf5def0c252252
Matt Mulit <mattmulit@hotmail.com>
2020-08-25 10:21:14 -0400

    up and down arrows now cycle li highlighting, but feature to auto-complete/set background not yet connected

--- app/renderer.js
+++ app/renderer.js
@@ -5,6 +5,7 @@ const list = document.querySelector('#file-list');
 
 let firstFile = '';
 let inputValue = '';
+// let currentFiles = [];
 
 showFiles = (files) => {
     for (i = 0; i < files.length; i++) {
@@ -22,8 +23,54 @@ showFiles = (files) => {
 };
 
 input.addEventListener('keyup', (event) => {
-    inputValue = input.value; // double check/rework all the other input.value inside cases below
+
+    inputValue = input.value; // double check/rework inputValue/input.value below
+
+    highlightLi = (arrowKey) => {
+
+        const listFiles = list.getElementsByTagName('li');
+        let increment = 0;
+        let newIndex = 0;
+        let highlightedIndex = undefined;
+
+        if (arrowKey == 'ArrowDown') {
+            increment++;
+        } else {
+            increment--;
+        }
+
+
+        for (var i = 0; i < listFiles.length; i++) {
+            if (listFiles[i].hasAttribute('style')) {
+                highlightedIndex = i;
+                document.getElementById(listFiles[i].id).removeAttribute('style');
+                break;
+            } else {
+                highlightedIndex = undefined;
+            }
+        }
+
+        // convert into switch, or if/else for seperate logic ok?
+        if (increment == 1 && highlightedIndex == undefined) { // when down is pushed and nothing is highlighted
+            newIndex = listFiles.length - 1;
+        } else if (increment == -1 && highlightedIndex == undefined) { // when up is pushed and nothing is highlighted
+            newIndex = 0;
+        } else if (increment == -1 && highlightedIndex == 0) { // when up is pushed and the top line is highlighted
+            newIndex = 0;
+        } else if (increment == 1 && highlightedIndex == listFiles.length - 1) { // when down is pushed and the bottom line is highlighted
+            newIndex = listFiles.length - 1;
+        } else {
+            newIndex = highlightedIndex + increment; //, else move up or down through the rest of the list
+        }
+
+        document.getElementById(listFiles[newIndex].id).style.backgroundColor = "#2d421b";
+    };
+
     switch (event.key) {
+        case 'ArrowDown':
+        case 'ArrowUp':
+            highlightLi(event.key);
+            break;
         case 'Enter':
             ipcRenderer.send('input-change', input.value);
             break;
@@ -35,6 +82,7 @@ input.addEventListener('keyup', (event) => {
         case 'Backspace':
             if (input.value == '') {
                 list.innerHTML = '';
+                // currentFiles = '';
             } else {
                 ipcRenderer.send('key-press', inputValue);
             }
@@ -49,6 +97,7 @@ input.addEventListener('keyup', (event) => {
             inputValue = input.value;
     }
 });
+
 input.addEventListener('click', () => {
     if (input.value == '') {
         ipcRenderer.send('input-clicked');
@@ -60,6 +109,7 @@ list.addEventListener('click', (event) => {
 });
 
 ipcRenderer.on('all-files', (_, allFiles) => {
+    // currentFiles = allFiles;
     if (list.innerHTML == '') {
         showFiles(allFiles);
         input.setAttribute('placeholder', "Click input to hide files");
@@ -69,6 +119,7 @@ ipcRenderer.on('all-files', (_, allFiles) => {
     }
 });
 ipcRenderer.on('files-match', (_, matchingFiles) => {
+    // currentFiles = matchingFiles;
     firstFile = matchingFiles[0];
     input.value = inputValue;
     list.innerHTML = '';
